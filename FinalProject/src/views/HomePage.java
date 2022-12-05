5808f9c3ade9687851e36bf86fa02f8604208c75
Apoorva Jain <jain.apo@northeastern.edu>
2022-12-05 16:48:25 -0500

    feat: Added Home Page layout.

---
+++ FinalProject/src/views/HomePage.java
@@ -0,0 +1,13 @@
+package views;
+
+import javax.swing.*;
+
+public class HomePage extends BaseFrame {
+    private JPanel mainPanel;
+
+    HomePage() {
+        super();
+        setContentPane(mainPanel);
+        
+    }
+}
