e302517d7f27e7f9acc42582077d85169498b942
Apoorva Jain <jain.apo@northeastern.edu>
2022-12-05 17:43:22 -0500

    feat: Added Logout button

--- FinalProject/src/views/HomePage.java
+++ FinalProject/src/views/HomePage.java
@@ -1,6 +1,10 @@
 package views;
 
+import domain.Application;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class HomePage extends BaseFrame {
     private JPanel mainPanel;
@@ -8,9 +12,17 @@ public class HomePage extends BaseFrame {
     private JButton jobPortalButton;
     private JButton collegePortalButton;
     private JButton trainingPortalButton;
+    private JButton logoutButton;
 
     HomePage() {
         super();
         setContentPane(mainPanel);
+        logoutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Application.setCurrentlyLoggedInPerson(null);
+                switchToWindow(new LoginPage());
+            }
+        });
     }
 }
