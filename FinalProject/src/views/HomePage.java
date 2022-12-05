66a8dfd154ab453e8732a97944b40069dd601303
Apoorva Jain <jain.apo@northeastern.edu>
2022-12-05 16:51:26 -0500

    feat: Added links to various enterprises in Home page.

--- FinalProject/src/views/HomePage.java
+++ FinalProject/src/views/HomePage.java
@@ -4,10 +4,14 @@ import javax.swing.*;
 
 public class HomePage extends BaseFrame {
     private JPanel mainPanel;
+    private JButton companyPortalButton;
+    private JButton jobPortalButton;
+    private JButton collegePortalButton;
+    private JButton trainingPortalButton;
 
     HomePage() {
         super();
         setContentPane(mainPanel);
-        
+
     }
 }
