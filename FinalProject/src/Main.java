c2890c27d8e878374aa9f14905deab32391ac9bc
Sharun Kumar Kakkad Sasikumar <kakkadsasikumar.s@northeastern.edu>
2022-12-05 16:12:36 -0500

    feat: added person class and updated the database with the persons table

--- FinalProject/src/Main.java
+++ FinalProject/src/Main.java
@@ -1,8 +1,18 @@
+import domain.Application;
 import views.LoginPage;
+
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        try {
+            var persons = Application.Database.getPersons();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
 
         new LoginPage().setVisible(true);
     }
