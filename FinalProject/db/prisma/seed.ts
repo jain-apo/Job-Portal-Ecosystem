2c223aaccb682711c86fd868ba65de095874404a
Sharun Kumar Kakkad Sasikumar <kakkadsasikumar.s@northeastern.edu>
2022-12-09 14:05:13 -0500

    feat: database setup and generated entities for the same

--- FinalProject/db/prisma/seed.ts
+++ FinalProject/db/prisma/seed.ts
@@ -7,12 +7,13 @@ async function main() {
         firstName: 'Sharun',
         lastName: 'Kumar',
         username: 'sharun',
+        phone: '9043133610',
         password: '$2a$12$NReSOnyEHTMjnmYwxLa7be49S4aIsSXi8ROHIeyg/3ZXFwEnmfEee',
         dateOfBirth: new Date('Sun Nov 05 1995 00:04:24 GMT-0500 (Eastern Standard Time)'),
         email: "sharunksplus@gmail.com",
     }
 
-    const sharun = await prisma.persons.upsert({
+    const sharun = await prisma.person.upsert({
         where: {
             email: "sharunksplus@gmail.com"
         },
---
+++ FinalProject/src/entities/Person.java
@@ -0,0 +1,110 @@
+package entities;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Person {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private int id;
+    @Basic
+    @Column(name = "firstName")
+    private String firstName;
+    @Basic
+    @Column(name = "lastName")
+    private String lastName;
+    @Basic
+    @Column(name = "dateOfBirth")
+    private Object dateOfBirth;
+    @Basic
+    @Column(name = "username")
+    private String username;
+    @Basic
+    @Column(name = "password")
+    private String password;
+    @Basic
+    @Column(name = "email")
+    private String email;
+    @Basic
+    @Column(name = "phone")
+    private String phone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Object getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Object dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName) && Objects.equals(dateOfBirth, person.dateOfBirth) && Objects.equals(username, person.username) && Objects.equals(password, person.password) && Objects.equals(email, person.email) && Objects.equals(phone, person.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, dateOfBirth, username, password, email, phone);
+    }
+}
