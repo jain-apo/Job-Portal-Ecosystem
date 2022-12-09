4cdd6bfc73cb033bd43bb3d7cb574fe68e9e999a
Apoorva Jain <jain.apo@northeastern.edu>
2022-12-09 17:21:57 -0500

    Feat: Added candidate table

--- FinalProject/src/enterprise/company/CandidatePage.java
+++ FinalProject/src/enterprise/company/CandidatePage.java
@@ -1,9 +1,42 @@
 package enterprise.company;
 
+import model.tableModel.CandidateTable;
 import views.BaseFrame;
 
 import javax.swing.*;
 
 public class CandidatePage extends BaseFrame {
     private JPanel mainPane;
+    private JTable candidate;
+
+    public CandidatePage() {
+        super();
+        setContentPane(mainPane);
+        displayPeople();
+    }
+
+    private void displayPeople() {
+        CandidateTable model = new CandidateTable();
+
+//        var details = Application.PersonsDirectory.getPerson();
+//
+//        for (var person : details) {
+//            Object[] data = new Object[]{
+//                    person.getId(),
+//                    person.getFullName(),
+//                    DateHelper.formatDate(person.getDateOfBirth(), "MMM-dd yyyy"),
+//                    person.getRole(),
+//                    "✖",
+//                    "✖",
+//            };
+//
+//            model.addRow(data);
+//        }
+//
+//        candidate.setModel(model);
+//
+//        TableHelpers.centerColumn(people, 0);
+//        TableHelpers.centerColumn(people, 4);
+//        TableHelpers.centerColumn(people, 5);
+    }
 }
---
+++ FinalProject/src/model/tableModel/CandidateTable.java
@@ -0,0 +1,17 @@
+package model.tableModel;
+
+import javax.swing.table.DefaultTableModel;
+
+public class CandidateTable extends DefaultTableModel {
+    public CandidateTable() {
+        super(new Object[][]{}, new String[]{"Id", "Full Name", "Date of Birth", "Role", "[edit]", "[delete]"});
+    }
+
+    public boolean isCellEditable(int row, int column) {
+        return false;
+    }
+
+    public Class getColumnClass(int column) {
+        return getValueAt(0, column).getClass();
+    }
+}
