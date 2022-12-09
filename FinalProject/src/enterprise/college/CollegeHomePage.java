cee69729b26ae281ba574ec0e2a846819217a6ab
Apoorva Jain <jain.apo@northeastern.edu>
2022-12-09 16:27:12 -0500

    feat: removed unused buttons

--- FinalProject/src/enterprise/college/CollegeHomePage.java
+++ FinalProject/src/enterprise/college/CollegeHomePage.java
@@ -3,21 +3,13 @@ package enterprise.college;
 import views.BaseFrame;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class CollegeHomePage extends BaseFrame {
-    private JButton interviewsButton;
     private JPanel mainPane;
-    private JButton candidateButton;
 
     public CollegeHomePage() {
         super();
         setContentPane(mainPane);
-        candidateButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-            }
-        });
+
     }
 }
