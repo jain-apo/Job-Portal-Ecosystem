41d1f1a2309bd8b9a05a9597dd33da1f01e92144
Sharun Kumar Kakkad Sasikumar <kakkadsasikumar.s@northeastern.edu>
2022-12-09 21:59:02 -0500

    added the models

--- FinalProject/db/prisma/seed.ts
+++ FinalProject/db/prisma/seed.ts
@@ -1,9 +1,12 @@
-import { PrismaClient } from '@prisma/client'
+import { Person, Prisma, PrismaClient, Role } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
 
-    let sharunData = {
+
+    const admin = await addRole({ name: "ADMIN" })
+
+    const sharun = await addUser({
         firstName: 'Sharun',
         lastName: 'Kumar',
         username: 'sharun',
@@ -11,15 +14,9 @@ async function main() {
         password: '$2a$12$NReSOnyEHTMjnmYwxLa7be49S4aIsSXi8ROHIeyg/3ZXFwEnmfEee',
         dateOfBirth: new Date('Sun Nov 05 1995 00:04:24 GMT-0500 (Eastern Standard Time)'),
         email: "sharunksplus@gmail.com",
-    }
-
-    const sharun = await prisma.person.upsert({
-        where: {
-            email: "sharunksplus@gmail.com"
-        },
-        update: sharunData,
-        create: sharunData,
     })
+
+    addPersonRole(sharun, admin)
 
     console.log({ sharun })
 }
@@ -32,3 +29,48 @@ main()
         await prisma.$disconnect()
         process.exit(1)
     })
+
+async function addRole(role: Prisma.RoleCreateInput) {
+    return await prisma.role.upsert({
+        where: {
+            name: role.name
+        },
+        update: role,
+        create: role,
+    })
+}
+
+async function addUser(user: Prisma.PersonCreateInput) {
+    return await prisma.person.upsert({
+        where: {
+            email: user.email
+        },
+        update: user,
+        create: user,
+    })
+}
+
+async function addPersonRole(person: Person, role: Role) {
+
+    const personRole: Prisma.PersonRoleCreateInput = {
+        person: { connect: { id: person.id } },
+        role: { connect: { id: role.id } }
+    }
+
+    return await prisma.personRole.upsert({
+        where: {
+            personId_roleId: {
+                personId: person.id,
+                roleId: role.id,
+            }
+        },
+        update: {
+            personId: person.id,
+            roleId: role.id,
+        },
+        create: {
+            personId: person.id,
+            roleId: role.id,
+        },
+    })
+}
