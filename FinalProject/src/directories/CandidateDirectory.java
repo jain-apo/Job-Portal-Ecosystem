25cec42e83960b2f8cc343aad30fd977bd3a53b4
Apoorva Jain <jain.apo@northeastern.edu>
2022-12-10 15:18:08 -0500

    feat: Added candidate directory.

---
+++ FinalProject/src/directories/CandidateDirectory.java
@@ -0,0 +1,38 @@
+package directories;
+
+import domain.Application;
+import models.JobCandidate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CandidateDirectory {
+
+    List<JobCandidate> jobCandidateList = new ArrayList<>();
+
+    public CandidateDirectory() {
+        loadFromDatabase();
+    }
+
+    public void loadFromDatabase() {
+        try {
+            this.jobCandidateList = Application.Database.JobCandidates.getAll();
+            System.out.println("There are " + jobCandidateList.stream().count() + " people in the database");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+      public List<JobCandidate> getCandidate() {
+        return jobCandidateList;
+    }
+
+    public JobCandidate getCandidateById(int id) {
+        return jobCandidateList.stream().filter(person -> person.getId() == id).findFirst().orElse(null);
+    }
+}
+
+}
