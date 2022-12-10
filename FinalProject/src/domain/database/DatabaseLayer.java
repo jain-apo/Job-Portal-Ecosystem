086dbc6501a546470e9574ed6f4d79ed4cc42508
Mihir Sheth <sheth.mi@northeastern.edu>
2022-12-10 03:54:25 -0500

    feat Added College Database

--- FinalProject/src/domain/database/DatabaseLayer.java
+++ FinalProject/src/domain/database/DatabaseLayer.java
@@ -4,4 +4,5 @@ public class DatabaseLayer {
     public PersonsDatabase Persons = new PersonsDatabase();
     public RoleDatabase Roles = new RoleDatabase();
     public CourseDatabase Courses = new CourseDatabase();
+    public CollegeStudentDatabase CollegeStudents = new CollegeStudentDatabase();
 }
