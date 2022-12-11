5a0f0f6a80ba1c4bea453212542bec22ce2a4269
Apoorva Jain <jain.apo@northeastern.edu>
2022-12-11 05:41:55 -0500

    feat: Added send request page in College portal.

---
+++ FinalProject/src/enterprise/college/SendRequestPage.java
@@ -0,0 +1,15 @@
+package enterprise.college;
+
+import views.BaseFrame;
+
+import javax.swing.*;
+
+public class SendRequestPage extends BaseFrame {
+
+    private JPanel p;
+
+    public SendRequestPage() {
+        super();
+        setContentPane(p);
+    }
+}
