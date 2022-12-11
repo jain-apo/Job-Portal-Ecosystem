9e1e30d7207cbeb2793f3aadf7bca766bcd7aca6
Sharun Kumar Kakkad Sasikumar <kakkadsasikumar.s@northeastern.edu>
2022-12-11 18:47:36 -0500

    feat: loaded the training module based on the role

--- FinalProject/src/enterprise/training/TrainingListPage.java
+++ FinalProject/src/enterprise/training/TrainingListPage.java
@@ -1,10 +1,17 @@
 package enterprise.training;
 
+import domain.Application;
+import domain.Roles;
+import helpers.TableHelpers;
+import models.tablemodels.TrainingModuleAdminTableModel;
+import models.tablemodels.TrainingModuleTableModel;
+import utils.Dialog;
 import views.BaseFrame;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
 
 public class TrainingListPage extends BaseFrame {
     private JPanel p;
@@ -14,10 +21,42 @@ public class TrainingListPage extends BaseFrame {
     private JTable modules;
     private JTextArea validationText;
     private JButton startTrainingButton;
+    private boolean isTrainer;
+    private boolean isTrainee;
 
     public TrainingListPage() {
         setupActionListeners();
+        setupRoles();
+        displayModules();
         setContentPane(p);
+    }
+
+    private void setupRoles() {
+        var person = Application.getCurrentlyLoggedInPerson();
+
+        isTrainer = person.getRoles().equals(Roles.TRAINING_SITE_ADMIN);
+        isTrainee = person.getRoles().equals(Roles.TRAINEE);
+
+        // TODO Roles Restriction
+    }
+
+    private void displayModules() {
+        try {
+
+            if (isTrainee) {
+                modules.setModel(new TrainingModuleTableModel().loadData(Application.Database.TrainingModules.getAll()));
+            }
+
+            if (isTrainer) {
+                modules.setModel(new TrainingModuleAdminTableModel().loadData(Application.Database.TrainingModules.getAll()));
+            }
+
+            for (int i = 0; i < modules.getColumnCount(); i++) {
+                TableHelpers.centerColumn(modules, i);
+            }
+        } catch (SQLException e) {
+            Dialog.error("Error loading training modules");
+        }
     }
 
     private void setupActionListeners() {
