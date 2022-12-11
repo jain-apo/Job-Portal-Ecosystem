ad3449d922def6883a744ba5e7e251ae66e0504a
Sharun Kumar Kakkad Sasikumar <kakkadsasikumar.s@northeastern.edu>
2022-12-11 18:46:22 -0500

    feat: TrainingModuleTableModel

---
+++ FinalProject/src/models/tablemodels/TrainingModuleTableModel.java
@@ -0,0 +1,17 @@
+package models.tablemodels;
+
+import models.TrainingModule;
+
+public class TrainingModuleTableModel extends BaseTableModel<TrainingModule> {
+    public TrainingModuleTableModel() {
+        super(new String[]{"Id", "Name", "Description", "[Start Module]"});
+    }
+
+    @Override
+    public Object[] rowMapping(TrainingModule item) {
+        return new Object[]{
+                item.getId(),
+                item.getName(),
+                "✖", "✖"};
+    }
+}
