b3b0bc439518268c1d67f9e4cac0388c67ebee4a
Apoorva Jain <jain.apo@northeastern.edu>
2022-12-11 17:54:41 -0500

    feat: Added role in job postings page.

--- FinalProject/src/enterprise/job/JobHomePage.java
+++ FinalProject/src/enterprise/job/JobHomePage.java
@@ -1,5 +1,6 @@
 package enterprise.job;
 
+import domain.Application;
 import views.BaseFrame;
 
 import javax.swing.*;
@@ -13,20 +14,38 @@ public class JobHomePage extends BaseFrame {
     private JPanel mainPane;
     private JLabel heading;
 
+    private boolean isStudent;
+    private boolean isHr;
+
     public JobHomePage() {
         super();
         setContentPane(p);
+        setupRoles();
         companyJobPostingsButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 new JobPostingsPage().setVisible(true);
             }
         });
-        myApplicationsButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new JobPostingsPage().setVisible(true);
-            }
-        });
+    }
+
+    private void setupRoles() {
+        var person = Application.getCurrentlyLoggedInPerson();
+
+        isStudent = person.getRoles().stream().anyMatch(role -> role.getName().equals("COLLEGE_STUDENT"));
+        isHr = person.getRoles().stream().anyMatch(role -> role.getName().equals("COMPANY_HR"));
+        System.out.println("isStudent" + isStudent);
+        System.out.println("isHr" + isHr);
+
+        if (!isHr) {
+//            addPersonPane.setVisible(false);
+//            addPersonButton.setVisible(false);
+//            myApplicationsButton.setVisible(true);
+
+        } else {
+//            jobPosterPanel.setVisible(true);
+//            addPersonButton.setVisible(true);
+//            myApplicationsButton.setVisible(false);
+        }
     }
 }
